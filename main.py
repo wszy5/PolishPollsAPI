1dd92aac28451c2d7eb73c87497a14e02f28cb9b
wszy5 <wikoszyman@wp.pl>
2023-03-04 12:49:31 +0100

    Stable version of scraping polls

--- main.py
+++ main.py
@@ -12,19 +12,35 @@ content = soup(website.content,'html.parser')
 
 polls = content.find_all("div",{"class":"section_polls_row"})[1:]
 
+responses = []
+
 for poll in polls:
-    results = {}
+    result = {}
     try:
         date = poll.find("div",{"class":"section_polls_term"}).text
-        results['date'] = date
+        result['date'] = date
+
         company = poll.find("a").text
-        results['company'] = company
+        result['company'] = company
+
         sample = poll.find("div",{"class":"section_polls_sample desktop"}).text
-        results['sample'] = sample
+        result['sample'] = sample
+
         details = {}
-        parties = poll.find_all("div")[4:]
-        for party in parties:
-            details['pis'] = ""
-            pass
+
+        parties = poll.find_all("div",{"class":"section_polls_data"})
+        details['lewica'] = parties[0].text
+        details['ko'] = parties[1].text
+        details['p2050'] = parties[2].text
+        details['psl'] = parties[3].text
+        details['ap'] = parties[4].text
+        details['w'] = parties[5].text
+        details['pis'] = parties[6].text
+        details['konfa'] = parties[7].text
+        
+        result['details'] = details
+        
+        responses.append(result)
     except:
         continue
+print(responses)
